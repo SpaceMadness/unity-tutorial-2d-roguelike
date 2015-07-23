216a556a0796c09952e59615b31a2fdf80f327d6
Alex Lementuev <a.lementuev@gmail.com>
2015-07-22 17:49:33 -0700

    Added commands for days management

---
+++ Assets/Editor/Commands.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Completed;
+
+using LunarPlugin;
+
+[CCommand("restart", Description="Restarts current day")]
+class Cmd_restart : CPlayModeCommand
+{
+	void Execute()
+	{
+		GameManager.instance.RestartDay();
+	}
+}
+
+[CCommand("next", Description="Starts next day")]
+class Cmd_next : CPlayModeCommand
+{
+	void Execute()
+	{
+		GameManager.instance.NextDay();
+	}
+}
+
+[CCommand("prev", Description="Starts prev day")]
+class Cmd_prev : CPlayModeCommand
+{
+	void Execute()
+	{
+		GameManager.instance.PrevDay();
+	}
+}
+
+[CCommand("day", Description="Starts Nth day")]
+class Cmd_day : CPlayModeCommand
+{
+	void Execute(int day)
+	{
+		GameManager.instance.StartDay(day);
+	}
+}
